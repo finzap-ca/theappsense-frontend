86f3fa1a24399b843544784051e0e0806ffff4dc
Mohit Tripathi <mohit@finzap.ca>
2026-06-29 04:49:27 +0200

    Track Tawk chat starts

--- components/integrations/TawkToScript.tsx
+++ components/integrations/TawkToScript.tsx
@@ -6,11 +6,43 @@ export function TawkToScript() {
   if (!HAS_TAWK) return null;
 
   return (
-    <Script
-      id="tawk-to-widget"
-      strategy="afterInteractive"
-      src={`https://embed.tawk.to/${TAWK_PROPERTY_ID}/${TAWK_WIDGET_ID}`}
-      crossOrigin="anonymous"
-    />
+    <>
+      <Script id="tawk-chat-started-tracking" strategy="beforeInteractive">
+        {`
+          window.Tawk_API = window.Tawk_API || {};
+
+          var existingOnChatStarted = window.Tawk_API.onChatStarted;
+
+          window.Tawk_API.onChatStarted = function (event) {
+            if (typeof existingOnChatStarted === 'function') {
+              try {
+                existingOnChatStarted.apply(this, arguments);
+              } catch (error) {
+                console.error('[Tracking] Existing Tawk onChatStarted callback failed', error);
+              }
+            }
+
+            if (window.__tawkChatStartedTracked) {
+              return;
+            }
+
+            window.__tawkChatStartedTracked = true;
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+              event: 'tawk_chat_started',
+              chat_provider: 'tawk_to',
+              page_path: window.location.pathname,
+              chat_id: event && event.chatId ? event.chatId : undefined
+            });
+          };
+        `}
+      </Script>
+      <Script
+        id="tawk-to-widget"
+        strategy="afterInteractive"
+        src={`https://embed.tawk.to/${TAWK_PROPERTY_ID}/${TAWK_WIDGET_ID}`}
+        crossOrigin="anonymous"
+      />
+    </>
   );
 }
